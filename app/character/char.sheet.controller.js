93c851ea9692ac18406a644ad66a24e08bdbfa46
Eric Pesch <ericpes@umich.edu>
2016-02-08 00:41:28 -0800

    NEW: Physical Condition track for character

--- app/character/char.sheet.controller.js
+++ app/character/char.sheet.controller.js
@@ -8,22 +8,109 @@
     .module('char')
     .controller('CharSheetCtrl', ['$scope', '$log', 'Mongo', 'Session',
         function($scope, $log, Mongo, Session){
+            $scope.initiative = 0;
+            $scope.conditionPhysical = [];
+            $scope.conditionStun = [];
+
+            //"condition": {
+            //    "physical": {
+            //        "current": 8,
+            //            "max": 10
+            //    },
+            //    "stun": {
+            //        "current": 4,
+            //            "max": 5
+            //    },
+            //    "effects": []
+            //}
+
+            function setupPhysical(input){
+                for(var i = 0; i < input.condition.physical.max; i++){
+                    var currPhysical = {
+                        "index": input.condition.physical.max - i,
+                        "isChecked": (i < (input.condition.physical.max
+                                        - input.condition.physical.current)),
+                        "classes": (i < (input.condition.physical.max
+                                        - input.condition.physical.current) ? "condition-fill" : "")
+                    };
+
+                    $scope.conditionPhysical.push(currPhysical);
+                }
+            }
+
+            function setupStun(input){
+                for(var i = 0; i < input.condition.stun.max; i++){
+                    var currPhysical = {
+                        "index": input.condition.stun.max - i,
+                        "isChecked": (i < (input.condition.stun.max
+                                        - input.condition.stun.current)),
+                        "classes": (i < (input.condition.stun.max
+                                        - input.condition.stun.current) ? "condition-fill" : "")
+                    };
+
+                    $scope.conditionStun.push(currPhysical);
+                }
+            }
+
+            $scope.physicalClick = function(index){
+                var diff = $scope.char.condition.physical.max - index;
+                $log.log("Index " + index + " clicked");
+
+                for(var i = 0; i < $scope.char.condition.physical.max; i++){
+                    if(i <= diff){
+                        $scope.conditionPhysical[i].classes = "condition-fill";
+                        $scope.conditionPhysical[i].isChecked = true;
+                    } else {
+                        $scope.conditionPhysical[i].classes = "";
+                        $scope.conditionPhysical[i].isChecked = false;
+                    }
+                }
+            };
+
+            $scope.physicalHover = function(index){
+                var diff = $scope.char.condition.physical.max - index;
+                for(var i = 0; i < $scope.char.condition.physical.max; i++){
+                    if($scope.conditionPhysical[i].isChecked){
+                        if(i > diff) {
+                            $scope.conditionPhysical[i].classes = "condition-hover";
+                        } else {
+                            $scope.conditionPhysical[i].classes = "condition-fill";
+                        }
+                    } else if(i <= diff){
+                        $scope.conditionPhysical[i].classes = "condition-hover";
+                    } else {
+                        $scope.conditionPhysical[i].classes = "";
+                    }
+                }
+            };
+
+            $scope.physicalReset = function(){
+                for(var i = 0; i < $scope.char.condition.physical.max; i++){
+                    if($scope.conditionPhysical[i].isChecked){
+                        $scope.conditionPhysical[i].classes = "condition-fill";
+                    } else {
+                        $scope.conditionPhysical[i].classes = "";
+                    }
+                }
+            };
+
+            //Utility functions
             function calcInitiative(input){
                 if(input != null)
                 return input.attributes.physical.reaction + input.attributes.mental.intuition;
             }
 
-            $log.log("Current Character Id SHEET = ", Session.getCharacter());
-
-            $scope.initiative = 0;
-
+            //Setup character functions
             $scope.char = Mongo.get({
                 model: "characters",
                 id: Session.getCharacter()
             });
 
             $scope.char.$promise.then(function(data){
+                $log.log("Data retrieved - init page");
                 $scope.initiative = calcInitiative(data);
+                setupPhysical(data);
+                setupStun(data);
             });
 
             $scope.save = function(){
---
+++ app/shared/filters/tables.js
@@ -0,0 +1,40 @@
+/**
+ * Created by ep3998 on 2/7/2016.
+ */
+(function(){
+    'use strict';
+
+    angular.module('shadowrunnerApp')
+    .filter("numColFilter", function(){
+        return function(input, row, numColumns){
+            var returnArray = [];
+            console.log("row - " + row + " columns - " + numColumns);
+
+            for(var x = row * numColumns; x < row * numColumns + numColumns; x++){
+                var condition = input[x];
+                if(x < input.length){
+                    returnArray.push(condition);
+                }
+                //else{
+                //    var blankCell = {
+                //        "index": x,
+                //        "classes": "condition-block"
+                //    };
+                //    returnArray.push(blankCell); //this is used for the empty cells
+                //}
+            }
+            return returnArray;
+        }
+    })
+    .filter("numRowFilter", function(){
+        return function(input, numColumns){
+            var filtered = [];
+            for(var x = 0; x < input.length; x++){
+                if(x % numColumns === 0){
+                    filtered.push(filtered.length);
+                }
+            }
+            return filtered;
+        }
+    });
+})();
